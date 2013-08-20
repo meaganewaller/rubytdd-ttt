95d30887ed5d03a59a272fc0ab4e3f6873f01947
meaganewaller <meaganewaller@gmail.com>
2013-08-20 11:48:40 -0400

    deleting unneccessary files

--- lib/ttt.rb
+++
@@ -1,9 +0,0 @@
-class TicTacToe
-  def prompt_opponent_type
-    puts "Would you like to play against a (h)uman?"
-    opponent = $stdin.gets.chomp.downcase
-      if opponent == "h"
-        opponent = :human
-      end
-  end
-end
