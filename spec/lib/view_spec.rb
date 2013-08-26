9279f402a368a9c55b9072e7efae1ad1815cee6b
meaganewaller <meaganewaller@gmail.com>
2013-08-26 17:06:16 -0400

    change #place_mark to #make_move

--- spec/lib/view_spec.rb
+++ spec/lib/view_spec.rb
@@ -16,29 +16,29 @@ describe View do
   end
 
   it "converts board for view on commandline" do
-    test_place_marks([1,2], :player)
-    test_place_marks([5,8], :opponent)
+    test_make_moves([1,2], :player)
+    test_make_moves([5,8], :opponent)
     test_markers = {Board::BLANK => "_", :player => "X", :opponent => "O" }
     expected = ["_|X|X", "_|_|O", "_|_|O"]
     @view.board_for_view(@board, test_markers).should == expected
   end
 
   it "converts available spaces for view on commandline" do
-    test_place_marks([4,5], :player)
-    test_place_marks([3,6], :opponent)
+    test_make_moves([4,5], :player)
+    test_make_moves([3,6], :opponent)
     expected = ["1 2 3", "     ", "  8 9"]
     @view.available_spaces_for_view(@board).should == expected
   end
 
   it "has list of available opponents" do
-    expected = "[1: Human, 2: Easy Computer, 3: Impossible Computer]"
-    @view.player_opponent_list(["Human", "Easy Computer", "Impossible Computer"]).should == expected
+    expected = "[1: Human, 2: Easy Computer, 3: Unbeatable Computer]"
+    @view.player_opponent_list(["Human", "Easy Computer", "Unbeatable Computer"]).should == expected
   end
 
   private
-  def test_place_marks(spaces, mark)
+  def test_make_moves(spaces, mark)
     spaces.each do |space|
-      @board.place_mark(space, mark)
+      @board.make_move(space, mark)
     end
   end
 end
