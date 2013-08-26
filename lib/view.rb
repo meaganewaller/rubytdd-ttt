bcc33cb79c94911b2070c74c282509a0f8054332
meaganewaller <meaganewaller@gmail.com>
2013-08-26 17:05:02 -0400

    refactor #board_for_view #available_spaces_for_view and #player_opponent_list

--- lib/view.rb
+++ lib/view.rb
@@ -1,21 +1,25 @@
 require 'board'
 class View
+
   def set_output(output)
     @output = output
   end
 
   def board_for_view(board, hash)
-    board.spaces.each_slice(board.size).map { |row| row.map {|marker| hash[marker]}.join("|")}
+    board.spaces.each_slice(board.size).map do |row|
+      row.map { |marker| hash[marker] }.join("|")
+    end
   end
 
-
   def available_spaces_for_view(board)
-    board.spaces.map.with_index { |space, index|
+    board.spaces.map.with_index do |space, index|
       space.eql?(Board::BLANK) ? index + 1 : " "
-    }.each_slice(board.size).to_a.map {|row| row.join(" ")}
+    end.each_slice(board.size).map { |row| row.join(" ") }
   end
 
   def player_opponent_list(players)
-    "[%s]" % players.map.with_index { |player, index| "%d: %s" % [index+1, player]}.join(", ")
+    players.map.with_index { |player, index| "#{index + 1}: #{player}" }.join(', ')
+                                                                        .insert( 0, '[')
+                                                                        .insert(-1, ']')
   end
 end
